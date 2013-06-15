418c675944e3614e23d3cf812d60131bd0df5efc
meiskam <meiskam@gmail.com>
2013-06-15 06:03:10 -0400

    Initialization

---
+++ README.md
@@ -0,0 +1,6 @@
+mcBootstrap
+===========
+
+Bootstrap for Minecraft's new launcher (MinecraftDev)
+
+This project is created from decompiled class files.  All code copyright Mojang.
---
+++ src/main/java/LZMA/LzmaException.java
@@ -0,0 +1,10 @@
+package LZMA;
+
+public class LzmaException extends java.io.IOException {
+	public LzmaException() {}
+
+	public LzmaException (String msg) {
+		super(msg);
+	}
+}
+
